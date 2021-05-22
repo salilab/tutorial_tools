71ab8fe63d425bb23815e57d120bcb5f6020a239
Ben Webb <ben@salilab.org>
2021-05-21 22:04:34 -0700

    Use GitHub-provided conda

--- setup_travis.sh
+++ setup_travis.sh
@@ -3,14 +3,13 @@
 # Set up an environment to run tests under Travis CI (see toplevel .travis.yml)
 # or GitHub Actions (see toplevel .github/workflows/build.yml)
 
-if [ $# -ne 3 ]; then
-  echo "Usage: $0 conda_dir imp_branch python_version"
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 imp_branch python_version"
   exit 1
 fi
 
-conda_dir=$1
-imp_branch=$2
-python_version=$3
+imp_branch=$1
+python_version=$2
 temp_dir=$(mktemp -d)
 
 if [ ${imp_branch} = "develop" ]; then
@@ -21,18 +20,6 @@ fi
 
 cd ${temp_dir}
 
-# Use miniconda Python rather than the Travis environment
-
-# Clean up after a potential previous install failure
-rm -rf ${conda_dir}
-# Save on some downloading if the version is the same
-if [ "${python_version}" == "2.7" ]; then
-  wget https://repo.continuum.io/miniconda/Miniconda2-latest-Linux-x86_64.sh -O miniconda.sh
-else
-  wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
-fi
-bash miniconda.sh -b -p ${conda_dir}
-export PATH=${conda_dir}/bin:$PATH
 conda update --yes -q conda
 conda create --yes -q -n python${python_version} -c salilab python=${python_version} scipy matplotlib nose ${IMP_CONDA}
 
