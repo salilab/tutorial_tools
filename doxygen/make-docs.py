cf6e92039e4c066220b64c56ff4cbbac5a4b513c
Ben Webb <ben@salilab.org>
2021-06-02 10:36:48 -0700

    Update for IMP 2.15.0 release

--- doxygen/make-docs.py
+++ doxygen/make-docs.py
@@ -29,7 +29,7 @@ except ImportError:
     from urllib2 import urlopen
 
 # Latest IMP stable release
-IMP_STABLE_RELEASE = '2.14.0'
+IMP_STABLE_RELEASE = '2.15.0'
 
 # Absolute path to the top of the repository
 TOPDIR = os.path.abspath('..')
