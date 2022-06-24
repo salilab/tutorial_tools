e4b7c3ef713e37d4fb1183580b0bd6018c329b5b
Ben Webb <ben@salilab.org>
2022-06-23 19:23:32 -0700

    Update for IMP 2.17.0 release

--- doxygen/make-docs.py
+++ doxygen/make-docs.py
@@ -29,7 +29,7 @@ except ImportError:
     from urllib2 import urlopen
 
 # Latest IMP stable release
-IMP_STABLE_RELEASE = '2.16.0'
+IMP_STABLE_RELEASE = '2.17.0'
 
 # Absolute path to the top of the repository
 TOPDIR = os.path.abspath('..')
--- notebook/process_notebook.py
+++ notebook/process_notebook.py
@@ -17,7 +17,7 @@ import urllib.request
 
 
 # Latest IMP stable release
-IMP_STABLE_RELEASE = '2.16.0'
+IMP_STABLE_RELEASE = '2.17.0'
 
 # Path to doxygen directory (containing doxygen inputs)
 DOXDIR = os.path.abspath(os.path.join(os.path.dirname(__file__),
