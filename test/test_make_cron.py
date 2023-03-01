d3dac8a3dcdb9299c23878544e2b4e2ea25f7b29
Ben Webb <ben@salilab.org>
2023-02-28 17:39:06 -0800

    Use same Python to run make-cron script

--- test/test_make_cron.py
+++ test/test_make_cron.py
@@ -2,6 +2,7 @@ import unittest
 import subprocess
 import utils
 import os
+import sys
 
 TOPDIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 
@@ -9,7 +10,7 @@ class Tests(unittest.TestCase):
     def test_complete(self):
         """Test simple complete run of make-cron.py"""
         make_cron = os.path.join(TOPDIR, "make-cron.py")
-        subprocess.check_call([make_cron])
+        subprocess.check_call([sys.executable, make_cron])
 
 
 if __name__ == '__main__':
